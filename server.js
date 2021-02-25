0294c8ee4c07cac11e30217b68e38b87ace68c9b
Ben Gross <benrgross@gmail.com>
2021-02-25 12:26:01 -0500

    concert html to handlebars.

--- server.js
+++ server.js
@@ -1,15 +1,20 @@
 // Requiring necessary npm packages
 const express = require("express");
+const app = express();
 const session = require("express-session");
 // Requiring passport as we've configured it
 const passport = require("./config/passport");
-
 // Setting up port and requiring models for syncing
 const PORT = process.env.PORT || 8080;
 const db = require("./models");
 
+//require handlebars
+const exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 // Creating express app and configuring middleware needed for authentication
-const app = express();
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
@@ -21,17 +26,12 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 // Requiring our routes
-require("./routes/html-routes.js")(app);
-require("./routes/api-routes.js")(app);
+app.use(require("./routes/api-routes.js"));
+app.use(require("./routes/html-routes"));
 
 // Syncing our database and logging a message to the user upon success
-db.sequelize.sync().then(() => {
+db.sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => {
-    console.log(
-      "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
-      PORT,
-      PORT
-    );
+    console.log(`Your server is running on port ${PORT}`);
   });
 });
-console.log("butterball20");
