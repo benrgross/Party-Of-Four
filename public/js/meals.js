b2231e42b2cff3502c4142294c6988fe53494143
Ben Gross <benrgross@gmail.com>
2021-02-27 17:06:28 -0500

    commit before merge.

--- public/js/meals.js
+++ public/js/meals.js
@@ -13,7 +13,7 @@ console.log(userId);
 
 $("#create-meal").click(() => {
   console.log(userId);
-
+  $(".addMeal").toggle();
   $.post("/api/meals", {
     userId: userId
   }).catch(error => {
@@ -30,14 +30,11 @@ $(".add-ingredient").click(e => {
 
   const ingredientName = $(".addIngredient").val();
   console.log(ingredientName);
-  $.get(`/api/meals/${userId}`)
-    .then(data => {
-      console.log(data);
-      mealId = data.id;
-      postIngredient(mealId);
-    })
-    .then(mealId => console.log(mealId));
-
+  $.get(`/api/meals/${userId}`).then(data => {
+    console.log(data);
+    mealId = data.id;
+    postIngredient(mealId);
+  });
   const ingredientEl = $("<h6>");
   const newIngredient = ingredientEl.text(ingredientName);
   ingredientsList.append(newIngredient);
@@ -62,12 +59,15 @@ function postIngredient(mealId) {
     });
 }
 
-// // $("delete-ingredient").click(() => {
-//   $.delete(`/api/deletefrommeal`, {
-//       le mealID = $(this).attr(data-mealId)
-//       let text = $(this).siblings(".text").val();
-//       deleteName = $(this).text()
-//       id: mealId,
-//       name:
-//   };
-// });
+$("delete-ingredient").click(() => {
+  const mealID = $(this).attr("data-mealId");
+  const deleteName = $(this)
+    .siblings(".text")
+    .val();
+  console.log("mealID", mealID);
+  console.log("deleteName", deleteName);
+  $.delete("/api/deletefrommeal", {
+    id: mealID,
+    name: deleteName
+  });
+});
