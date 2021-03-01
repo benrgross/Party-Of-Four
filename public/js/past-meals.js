e3650b8052a9b6f767d1efe9838dab599310e438
Ben Gross <benrgross@gmail.com>
2021-02-28 19:49:11 -0500

    displat last three meals

--- public/js/past-meals.js
+++ public/js/past-meals.js
@@ -3,53 +3,92 @@ $(document).ready(() => {
   // and updates the HTML on the page
   $.get("/api/allmeals").then(data => {
     console.log(data);
-    const last3 = data.slice(0, 3).map(meal => {
-      console.log(meal.Ingredients);
-      return meal.Ingredients;
-    });
+    const last3 = data.slice(0, 3);
+    console.log("last3", last3);
 
+    console.log(data[0].Ingredients[0].name);
     const meal1 = last3.slice(0, 1);
     const meal2 = last3.slice(1, 2);
     const meal3 = last3.slice(2, 3);
-    console.log(meal1, meal2, meal3);
 
-    meal1.forEach(ingredient => {
+    console.log("meall1", meal3);
+
+    console.log(meal1[0].Ingredients.length);
+    for (let i = 0; i < meal1[0].Ingredients.length; i++) {
+      console.log("is this it", meal1[0].Ingredients[i].name);
       const ingredientEl = $("<h6>").addClass("title is-6");
+
       const watchlistBtn = $("<button>")
         .text("Add to Watchlist")
         .addClass("add-to-watch button is-warning")
-        .attr("data-meal", ingredient.MealIngredients.MealId)
-        .attr("data-name", ingredient.name);
+        .attr("data-meal", meal1[0].Ingredients[i].MealIngredients.MealId)
+        .attr("data-name", meal1[0].Ingredients[i].name);
+
       const ingredientDelBtn = $("<button>")
         .text("Delete")
         .attr("id", "deleteBtn")
         .addClass("delete-ingredient button is-danger is-outlined")
-        .attr("data-meal", ingredient.MealIngredients.MealId)
-        .attr("data-name", ingredient.name);
+        .attr("data-meal", meal1[0].Ingredients[i].MealIngredients.MealId)
+        .attr("data-name", meal1[0].Ingredients[i].name);
+
       const newIngredient = ingredientEl
-        .text(ingredient.name)
+        .text(meal1[0].Ingredients[i].name)
         .append(watchlistBtn)
         .append(ingredientDelBtn);
+
       $("#meal-1").append(newIngredient);
-    });
+    }
+
+    console.log(meal2[0].Ingredients.length);
+    for (let i = 0; i < meal2[0].Ingredients.length; i++) {
+      console.log("is this it", meal2[0].Ingredients[i].name);
+      const ingredientEl = $("<h6>").addClass("title is-6");
+
+      const watchlistBtn = $("<button>")
+        .text("Add to Watchlist")
+        .addClass("add-to-watch button is-warning")
+        .attr("data-meal", meal2[0].Ingredients[i].MealIngredients.MealId)
+        .attr("data-name", meal2[0].Ingredients[i].name);
+
+      const ingredientDelBtn = $("<button>")
+        .text("Delete")
+        .attr("id", "deleteBtn")
+        .addClass("delete-ingredient button is-danger is-outlined")
+        .attr("data-meal", meal2[0].Ingredients[i].MealIngredients.MealId)
+        .attr("data-name", meal2[0].Ingredients[i].name);
+
+      const newIngredient = ingredientEl
+        .text(meal2[0].Ingredients[i].name)
+        .append(watchlistBtn)
+        .append(ingredientDelBtn);
+
+      $("#meal-2").append(newIngredient);
+    }
+
+    console.log(meal3[0].Ingredients.length);
+    for (let i = 0; i < meal3[0].Ingredients.length; i++) {
+      console.log("is this it", meal3[0].Ingredients[i].name);
+      const ingredientEl = $("<h6>").addClass("title is-6");
+
+      const watchlistBtn = $("<button>")
+        .text("Add to Watchlist")
+        .addClass("add-to-watch button is-warning")
+        .attr("data-meal", meal3[0].Ingredients[i].MealIngredients.MealId)
+        .attr("data-name", meal3[0].Ingredients[i].name);
+
+      const ingredientDelBtn = $("<button>")
+        .text("Delete")
+        .attr("id", "deleteBtn")
+        .addClass("delete-ingredient button is-danger is-outlined")
+        .attr("data-meal", meal3[0].Ingredients[i].MealIngredients.MealId)
+        .attr("data-name", meal3[0].Ingredients[i].name);
+
+      const newIngredient = ingredientEl
+        .text(meal3[0].Ingredients[i].name)
+        .append(watchlistBtn)
+        .append(ingredientDelBtn);
+
+      $("#meal-3").append(newIngredient);
+    }
   });
 });
-
-//  for (let i = 0; i < data.Ingredients.length; i++) {
-//         const ingredientEl = $("<h6>").addClass("title is-6");
-//         const watchlistBtn = $("<button>")
-//           .text("Add to Watchlist")
-//           .addClass("add-to-watch button is-warning")
-//           .attr("data-meal", data.id)
-//           .attr("data-name", data.Ingredients[i].name);
-//         const ingredientDelBtn = $("<button>")
-//           .text("Delete")
-//           .attr("id", "deleteBtn")
-//           .addClass("delete-ingredient button is-danger is-outlined")
-//           .attr("data-meal", data.id)
-//           .attr("data-name", data.Ingredients[i].name);
-//         const newIngredient = ingredientEl
-//           .text(data.Ingredients[i].name)
-//           .append(watchlistBtn)
-//           .append(ingredientDelBtn);
-//         ingredientsList.append(newIngredient);
