ead89481370c997b4d3fad87118a9d32f28eaac1
Ben Gross <benrgross@gmail.com>
2021-03-03 16:37:08 -0500

    delte meal not working.

--- public/js/past-meals.js
+++ public/js/past-meals.js
@@ -2,191 +2,7 @@ let offset = 0;
 
 $(document).ready(() => {
   $("#past-three").hide();
-  // This file just does a GET request to figure out which user is logged in
-  // and updates the HTML on the page
-  $.get("/api/allmeals").then(data => {
-    const last3 = data.slice(0, 3);
-
-    const meal1 = last3.slice(0, 1);
-    const meal2 = last3.slice(1, 2);
-    const meal3 = last3.slice(2, 3);
-
-    if (meal1[0]) {
-      const dataDate1 = meal1[0].createdAt;
-      console.log("created", dataDate1);
-
-      const orderDate1 = dataDate1.substr(0, 10);
-      const dateArr1 = orderDate1.split("-");
-      dateArr1.push(dateArr1.shift());
-      const date1 = dateArr1.join("/");
-
-      const getTime1 = dataDate1
-        .substr(11)
-        .slice(0, 8)
-        .split(":");
-
-      let hour;
-      if (Number(getTime1[0] - 4) > 12) {
-        hour = (Number(getTime1[0] - 4) - 12).toString();
-        getTime1.push("pm");
-      } else {
-        hour = getTime1[0] - 4;
-        getTime1.push("am");
-      }
-
-      const time1 = hour + ":" + getTime1[1] + getTime1[3];
-      console.log(time1);
-
-      const dateEl1 = $("<h2>")
-        .addClass("title is-2")
-        .text(date1 + "  " + time1);
-      $("#meal-1").append(dateEl1);
-    }
-
-    if (meal2[0]) {
-      const dataDate2 = meal2[0].createdAt;
-      const orderDate2 = dataDate2.substr(0, 10);
-      const dateArr2 = orderDate2.split("-");
-      dateArr2.push(dateArr2.shift());
-      const date2 = dateArr2.join("/");
-      console.log("date2", date2);
-
-      const getTime2 = dataDate2
-        .substr(11)
-        .slice(0, 8)
-        .split(":");
-
-      if (Number(getTime2[0] - 4) > 12) {
-        hour = (Number(getTime2[0] - 4) - 12).toString();
-        getTime2.push("pm");
-      } else {
-        hour = getTime2[0] - 4;
-        getTime2.push("am");
-      }
-      console.log("hour", hour);
-      console.log("getTime2", getTime2);
-
-      const time2 = hour + ":" + getTime2[1] + getTime2[3];
-      console.log(time2);
-
-      const dateEl2 = $("<h2> ")
-        .addClass("title is-2")
-        .text(date2 + "  " + time2);
-      $("#meal-2").append(dateEl2);
-    }
-
-    if (meal3[0]) {
-      const dataDate3 = meal3[0].createdAt;
-      const orderDate3 = dataDate3.substr(0, 10);
-      const dateArr3 = orderDate3.split("-");
-      dateArr3.push(dateArr3.shift());
-      const date3 = dateArr3.join("/");
-
-      const getTime3 = dataDate3
-        .substr(11)
-        .slice(0, 8)
-        .split(":");
-
-      if (Number(getTime3[0] - 4) > 12) {
-        hour = (Number(getTime3[0] - 4) - 12).toString();
-        getTime3.push("pm");
-      } else {
-        hour = getTime3[0] - 4;
-        getTime3.push("am");
-      }
-
-      const time3 = hour + ":" + getTime3[1] + getTime3[3];
-
-      const dateEl3 = $("<h2>")
-        .addClass("title is-2")
-        .text(date3 + "  " + time3);
-      $("#meal-3").append(dateEl3);
-    }
-
-    console.log(meal1[0].Ingredients.length);
-    for (let i = 0; i < meal1[0].Ingredients.length; i++) {
-      const ingredientEl = $("<h4>").addClass("title is-4");
-
-      const watchlistBtn = $("<button>")
-        .text("Add to Watchlist")
-        .addClass("add-to-watch button is-link is-light")
-        .attr("data-meal", meal1[0].Ingredients[i].MealIngredients.MealId)
-        .attr("data-name", meal1[0].Ingredients[i].name);
-
-      const ingredientDelBtn = $("<button>")
-        .text("Delete")
-        .attr("id", "deleteBtn")
-        .addClass("delete-ingredient button is-danger is-outlined")
-        .attr("data-meal", meal1[0].Ingredients[i].MealIngredients.MealId)
-        .attr("data-name", meal1[0].Ingredients[i].name);
-
-      const newIngredient = ingredientEl
-        .text(
-          meal1[0].Ingredients[i].name.charAt(0).toUpperCase() +
-            meal1[0].Ingredients[i].name.slice(1)
-        )
-        .append(watchlistBtn)
-        .append(ingredientDelBtn);
-
-      $("#meal-1").append(newIngredient);
-    }
-
-    for (let i = 0; i < meal2[0].Ingredients.length; i++) {
-      console.log("is this it", meal2[0].Ingredients[i].name);
-      const ingredientEl = $("<h4>").addClass("title is-4");
-
-      const watchlistBtn = $("<button>")
-        .text("Add to Watchlist")
-        .addClass("add-to-watch button is-link is-light")
-        .attr("data-meal", meal2[0].Ingredients[i].MealIngredients.MealId)
-        .attr("data-name", meal2[0].Ingredients[i].name);
-
-      const ingredientDelBtn = $("<button>")
-        .text("Delete")
-        .attr("id", "deleteBtn")
-        .addClass("delete-ingredient button is-danger is-outlined")
-        .attr("data-meal", meal2[0].Ingredients[i].MealIngredients.MealId)
-        .attr("data-name", meal2[0].Ingredients[i].name);
-
-      const newIngredient = ingredientEl
-        .text(
-          meal2[0].Ingredients[i].name.charAt(0).toUpperCase() +
-            meal2[0].Ingredients[i].name.slice(1)
-        )
-        .append(watchlistBtn)
-        .append(ingredientDelBtn);
-
-      $("#meal-2").append(newIngredient);
-    }
-
-    for (let i = 0; i < meal3[0].Ingredients.length; i++) {
-      console.log("is this it", meal3[0].Ingredients[i].name);
-      const ingredientEl = $("<h4>").addClass("title is-4");
-
-      const watchlistBtn = $("<button>")
-        .text("Add to Watchlist")
-        .addClass("add-to-watch button is-link is-light")
-        .attr("data-meal", meal3[0].Ingredients[i].MealIngredients.MealId)
-        .attr("data-name", meal3[0].Ingredients[i].name);
-
-      const ingredientDelBtn = $("<button>")
-        .text("Delete")
-        .attr("id", "deleteBtn")
-        .addClass("delete-ingredient button is-danger is-outlined")
-        .attr("data-meal", meal3[0].Ingredients[i].MealIngredients.MealId)
-        .attr("data-name", meal3[0].Ingredients[i].name);
-
-      const newIngredient = ingredientEl
-        .text(
-          meal3[0].Ingredients[i].name.charAt(0).toUpperCase() +
-            meal3[0].Ingredients[i].name.slice(1)
-        )
-        .append(watchlistBtn)
-        .append(ingredientDelBtn);
-
-      $("#meal-3").append(newIngredient);
-    }
-  });
+  displayThree(offset);
 });
 
 // adds ingredient to watch list
@@ -219,9 +35,10 @@ $(".past-meals").on("click", ".delete-ingredient", e => {
     },
     body: JSON.stringify(deleteObject)
   })
-    .then(() => {
-      location.reload();
+    .then(data => {
+      console.log("data", data);
     })
+
     .catch(err => console.error(err));
 });
 
@@ -232,6 +49,22 @@ $("#next-three").click(() => {
   } else {
     $("#past-three").hide();
   }
+  displayThree(offset);
+});
+
+$("#past-three").click(() => {
+  offset -= 3;
+
+  if (offset >= 3) {
+    $("#past-three").show();
+  } else {
+    $("#past-three").hide();
+  }
+
+  displayThree(offset);
+});
+
+const displayThree = offset => {
   $.get(`/api/allmeals/${offset}`).then(data => {
     $("#meal-1").empty();
     $("#meal-2").empty();
@@ -415,196 +248,26 @@ $("#next-three").click(() => {
       $("#meal-3").append(newIngredient);
     }
   });
-});
+};
 
-$("#past-three").click(() => {
-  offset -= 3;
-
-  if (offset >= 3) {
-    $("#past-three").show();
-  } else {
-    $("#past-three").hide();
-  }
-
-  $.get(`/api/allmeals/${offset}`).then(data => {
-    $("#meal-1").empty();
-    $("#meal-2").empty();
-    $("#meal-3").empty();
-    const meal1 = data.slice(0, 1);
-    const meal2 = data.slice(1, 2);
-    const meal3 = data.slice(2, 3);
-
-    if (meal1[0]) {
-      const dataDate1 = meal1[0].createdAt;
-      console.log("created", dataDate1);
-
-      const orderDate1 = dataDate1.substr(0, 10);
-      const dateArr1 = orderDate1.split("-");
-      dateArr1.push(dateArr1.shift());
-      const date1 = dateArr1.join("/");
-
-      const getTime1 = dataDate1
-        .substr(11)
-        .slice(0, 8)
-        .split(":");
-
-      let hour;
-      if (Number(getTime1[0] - 4) > 12) {
-        hour = (Number(getTime1[0] - 4) - 12).toString();
-        getTime1.push("pm");
-      } else {
-        hour = getTime1[0] - 4;
-        getTime1.push("am");
-      }
-
-      const time1 = hour + ":" + getTime1[1] + getTime1[3];
-      console.log(time1);
-
-      const dateEl1 = $("<h2>")
-        .addClass("title is-2")
-        .text(date1 + "  " + time1);
-      $("#meal-1").append(dateEl1);
-    }
-
-    if (meal2[0]) {
-      const dataDate2 = meal2[0].createdAt;
-      const orderDate2 = dataDate2.substr(0, 10);
-      const dateArr2 = orderDate2.split("-");
-      dateArr2.push(dateArr2.shift());
-      const date2 = dateArr2.join("/");
-      console.log("date2", date2);
-
-      const getTime2 = dataDate2
-        .substr(11)
-        .slice(0, 8)
-        .split(":");
-
-      if (Number(getTime2[0] - 4) > 12) {
-        hour = (Number(getTime2[0] - 4) - 12).toString();
-        getTime2.push("pm");
-      } else {
-        hour = getTime2[0] - 4;
-        getTime2.push("am");
-      }
-
-      const time2 = hour + ":" + getTime2[1] + getTime2[3];
-      console.log(time2);
-
-      const dateEl2 = $("<h2> ")
-        .addClass("title is-2")
-        .text(date2 + "  " + time2);
-      $("#meal-2").append(dateEl2);
-    }
-
-    if (meal3[0]) {
-      const dataDate3 = meal3[0].createdAt;
-      const orderDate3 = dataDate3.substr(0, 10);
-      const dateArr3 = orderDate3.split("-");
-      dateArr3.push(dateArr3.shift());
-      const date3 = dateArr3.join("/");
-
-      const getTime3 = dataDate3
-        .substr(11)
-        .slice(0, 8)
-        .split(":");
-
-      if (Number(getTime3[0] - 4) > 12) {
-        hour = (Number(getTime3[0] - 4) - 12).toString();
-        getTime3.push("pm");
-      } else {
-        hour = getTime3[0] - 4;
-        getTime3.push("am");
-      }
-
-      const time3 = hour + ":" + getTime3[1] + getTime3[3];
-
-      const dateEl3 = $("<h2>")
-        .addClass("title is-2")
-        .text(date3 + "  " + time3);
-      $("#meal-3").append(dateEl3);
-    }
-
-    console.log(meal1[0].Ingredients.length);
-    for (let i = 0; i < meal1[0].Ingredients.length; i++) {
-      const ingredientEl = $("<h4>").addClass("title is-4");
-
-      const watchlistBtn = $("<button>")
-        .text("Add to Watchlist")
-        .addClass("add-to-watch button is-link is-light")
-        .attr("data-meal", meal1[0].Ingredients[i].MealIngredients.MealId)
-        .attr("data-name", meal1[0].Ingredients[i].name);
-
-      const ingredientDelBtn = $("<button>")
-        .text("Delete")
-        .attr("id", "deleteBtn")
-        .addClass("delete-ingredient button is-danger is-outlined")
-        .attr("data-meal", meal1[0].Ingredients[i].MealIngredients.MealId)
-        .attr("data-name", meal1[0].Ingredients[i].name);
-
-      const newIngredient = ingredientEl
-        .text(
-          meal1[0].Ingredients[i].name.charAt(0).toUpperCase() +
-            meal1[0].Ingredients[i].name.slice(1)
-        )
-        .append(watchlistBtn)
-        .append(ingredientDelBtn);
-
-      $("#meal-1").append(newIngredient);
-    }
-    for (let i = 0; i < meal2[0].Ingredients.length; i++) {
-      console.log("is this it", meal2[0].Ingredients[i].name);
-      const ingredientEl = $("<h4>").addClass("title is-4");
-
-      const watchlistBtn = $("<button>")
-        .text("Add to Watchlist")
-        .addClass("add-to-watch button is-link is-light")
-        .attr("data-meal", meal2[0].Ingredients[i].MealIngredients.MealId)
-        .attr("data-name", meal2[0].Ingredients[i].name);
-
-      const ingredientDelBtn = $("<button>")
-        .text("Delete")
-        .attr("id", "deleteBtn")
-        .addClass("delete-ingredient button is-danger is-outlined")
-        .attr("data-meal", meal2[0].Ingredients[i].MealIngredients.MealId)
-        .attr("data-name", meal2[0].Ingredients[i].name);
-
-      const newIngredient = ingredientEl
-        .text(
-          meal2[0].Ingredients[i].name.charAt(0).toUpperCase() +
-            meal2[0].Ingredients[i].name.slice(1)
-        )
-        .append(watchlistBtn)
-        .append(ingredientDelBtn);
-
-      $("#meal-2").append(newIngredient);
-    }
-
-    for (let i = 0; i < meal3[0].Ingredients.length; i++) {
-      console.log("is this it", meal3[0].Ingredients[i].name);
-      const ingredientEl = $("<h4>").addClass("title is-4");
-
-      const watchlistBtn = $("<button>")
-        .text("Add to Watchlist")
-        .addClass("add-to-watch button is-link is-light")
-        .attr("data-meal", meal3[0].Ingredients[i].MealIngredients.MealId)
-        .attr("data-name", meal3[0].Ingredients[i].name);
-
-      const ingredientDelBtn = $("<button>")
-        .text("Delete")
-        .attr("id", "deleteBtn")
-        .addClass("delete-ingredient button is-danger is-outlined")
-        .attr("data-meal", meal3[0].Ingredients[i].MealIngredients.MealId)
-        .attr("data-name", meal3[0].Ingredients[i].name);
-
-      const newIngredient = ingredientEl
-        .text(
-          meal3[0].Ingredients[i].name.charAt(0).toUpperCase() +
-            meal3[0].Ingredients[i].name.slice(1)
-        )
-        .append(watchlistBtn)
-        .append(ingredientDelBtn);
-
-      $("#meal-3").append(newIngredient);
+const deleteMeal = mealID => {
+  $.get(`/api/meal/${mealID}`).then(data => {
+    console.log(data);
+    if (data === null) {
+      console.log("it works");
+      console.log(mealID);
+      const deleteMeal = {
+        id: mealID
+      };
+      fetch("/api/deletemeal", {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(deleteMeal)
+      });
+    } else {
+      console.log(error);
     }
   });
-});
+};
