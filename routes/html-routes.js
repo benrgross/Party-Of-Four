1c63f35cabc93067fec689f33c11574dbf76ad80
Ben Gross <benrgross@gmail.com>
2021-04-02 14:21:17 -0400

    log out

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -21,8 +21,6 @@ Router.get("/login", (req, res) => {
   res.render("login", req.user);
 });
 
-// Here we've add our isAuthenticated middleware to this route.
-// If a user who is not logged in tries to access this route they will be redirected to the signup page
 Router.get("/members", isAuthenticated, (req, res) => {
   res.render("members");
 });
